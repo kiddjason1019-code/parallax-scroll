5f8ecad49cf2578ca3414e6057fbd0a715b4a11e
kiddjason1019-code <kiddjason1019@gmail.com>
2025-12-06 15:51:58 +0800

    Localize page content to Chinese

--- script.js
+++ script.js
@@ -81,8 +81,8 @@ function consoleText() {
 		"margin: 6px 0 6px 14px",
 	].join(";");
 	const styles2 = ["font-size: 14px", "padding: 16px", "margin: 6px 0 6px 0", "color: rgba(238,58,136,1)"].join(";");
-	console.log("%cHello World! I'm Emmanuel.", styles);
-	console.log("%cThank you for checking out my work!", styles2);
+        console.log("%c哈囉！我是 Emmanuel。", styles);
+        console.log("%c感謝你欣賞我的作品！", styles2);
 	const gradient = [
 		"font-size: 14px",
 		"color: #fff",
@@ -93,11 +93,11 @@ function consoleText() {
 		"background: rgba(238,58,136,1)",
 		"background: linear-gradient( 109.6deg, rgba(238,58,136,1) 11.2%, rgba(128,162,245,1) 91.1% )",
 	].join(";");
-	console.log("%cPortfolio%chttps://bit.ly/3QQr1Ux", gradient, styles2);
-	console.log("%cLinkedin %chttps://bit.ly/3cygAD4", gradient, styles2);
-	console.log("%cGithub   %chttps://bit.ly/3iwQC6U", gradient, styles2);
-	console.log("%cThe README   %chttps://bit.ly/3f28as1", gradient, styles2);
-	console.log("%cHave a wonderful day!", styles2);
+        console.log("%c作品集%chttps://bit.ly/3QQr1Ux", gradient, styles2);
+        console.log("%cLinkedIn %chttps://bit.ly/3cygAD4", gradient, styles2);
+        console.log("%cGitHub   %chttps://bit.ly/3iwQC6U", gradient, styles2);
+        console.log("%cREADME   %chttps://bit.ly/3f28as1", gradient, styles2);
+        console.log("%c祝你有美好的一天！", styles2);
 }
 
 swRegistration();
